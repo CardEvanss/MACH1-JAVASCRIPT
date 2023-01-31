5b168dd8aacb9ce3ba72601e68ebf4c0987a5dec
Evanderson Oliveira <evanderson.santana@hotmail.com>
2023-01-30 21:36:53 -0300

    Aula 08 / Respostas das atividades

---
+++ aula8_js-main/script2.js
@@ -0,0 +1,18 @@
+
+let produtos = ['Sem Cadastro', 'Leite', 'Ovo', 'Farinha', 'Milho', 'Café']
+
+let codigo = prompt('Digite o código para exibir qual posição está seu produto')
+
+if (codigo>=produtos.length) {
+    confirm(`${produtos[0]}. Deseja incluir um novo produto?`)
+    if (confirm) {
+        let novoProduto = prompt('Digite o nome do novo produto:')
+        produtos.push(novoProduto)
+        alert(`O novo produto incluído é ${novoProduto}. O código deste novo produto é  ${[(produtos.length-1)]}`)
+        window.location.reload()
+    } else {
+
+    }
+}else{
+    alert(`O produto corresponde ao código ${codigo} é ${produtos[codigo]}.`)
+} 
