d7883d942bdb2ab6170ecb92e4c92a55697544c8
Evanderson Oliveira <evanderson.santana@hotmail.com>
2023-01-31 20:42:08 -0300

    Adição dos itens, da aula 9

---
+++ aula9_js-main/aula9_js-main/script.js
@@ -0,0 +1,16 @@
+{
+    let alunos = ['Charle','Fernando','Lucas','Danilo']// Array do tipo vetor 
+    console.log(alunos)
+    console.log(alunos[1])
+    console.log(alunos[2])
+    let notas = [[9,8,10],[5,4,10],[10,9,8]] // Array do tipo matriz
+    console.log(notas)
+    console.log('Primeira Nota',notas[0][0])
+    console.log('Ultima linha e ultima coluna',notas[2][2])
+    //Adicionando uma linha na matriz
+    notas.push([10,10,10])
+    console.log(notas)
+    /* 3x3
+        matriz [3][3]
+    */
+}
---
+++ aula9_js-main/aula9_js-main/script2.js
@@ -0,0 +1,20 @@
+let tabela = [
+    [2,0,0],
+    [2,1,2],
+    [2,2,4],
+    [2,3,6],
+    [2,4,8],
+    [2,5,10],
+    [2,6,12],
+    [2,7,14],
+    [2,8,16],
+    [2,9,18],
+    [2,10,20],
+];
+
+console.log(tabela)
+// B
+alert("A representação logica dessa tabela é uma matriz bidimensional e é uma tabuada múltiplos de 2.");
+
+//C
+alert("Podemos chegar a conclusão que a terceira coluna é o resultado da multiplicação da primeira coluna com a segunda coluna!");
